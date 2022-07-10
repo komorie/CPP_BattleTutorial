85702d0e3ca5fc094ea0cdaf21104a247202275d
Komorie <widwin45@gmail.com>
2022-07-10 13:58:06 +0900

    init

---
+++ Source/CPP_BattleTutorial/CPP_BattleTutorial.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CPP_BattleTutorial.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CPP_BattleTutorial, "CPP_BattleTutorial" );
